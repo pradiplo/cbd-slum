e6eaf5286b00795db8a397105972d9044029e220
Pradipto <pradipto711@gmail.com>
2023-09-13 23:28:18 +0900

    radial scaling

--- plot.py
+++ plot.py
@@ -3,50 +3,170 @@ import matplotlib.pyplot as plt
 os.environ['USE_PYGEOS'] = '0'
 import geopandas as gpd
 import matplotlib as mpl
+import pandas as pd
+import numpy as np
+import pickle
+import gzip
 #mpl.style.use('classic')
 mpl.rcParams['mathtext.fontset'] = 'stix'
 mpl.rcParams['font.family'] = 'STIXGeneral'
 project='EPSG:4326'
-df_path = "./hthr30-100.json"
-city=gpd.read_file(df_path)
-print(city.columns)
+
+def rsquare(x, y, degree):
+    results = {}
+    coeffs = np.polyfit(x, y, degree)
+     # Polynomial Coefficients
+    results['polynomial'] = coeffs.tolist()
+    # r-squared
+    p = np.poly1d(coeffs)
+    # fit values, and mean
+    yhat = p(x)                         # or [p(z) for z in x]
+    ybar = np.sum(y)/len(y)          # or sum(y)/len(y)
+    ssreg = np.sum((yhat-ybar)**2)   # or sum([ (yihat - ybar)**2 for yihat in yhat])
+    sstot = np.sum((y - ybar)**2)    # or sum([ (yi - ybar)**2 for yi in y])
+    rsq = ssreg / sstot
+    return rsq
+
+def sami(y,x,m,b):
+    return y -  (x*m + b)
+
+def fitfunc(x,y):
+    m ,b = np.polyfit(x,y,1)
+    r2 = rsquare(x,y,1)
+    return m,b,r2
+
+def func(m,b, newX):
+    return newX*m + b
+
+def read_compressed(path): #buat buka file .gz di atas wk
+ 
+    with open(path, 'rb') as f:
+        compressed_data = f.read()
+        
+    
+    # Decompress data using gzip and pickle
+        decompressed_data = gzip.decompress(compressed_data)
+    
+    
+   # import pygeos as sp
+    
+    data = pickle.loads(decompressed_data)
+        
+   
+        #print(data)
+    gdf = gpd.GeoDataFrame.from_dict(data)
+
+    return gdf
+
+
+df_small_path = "/Users/tagawayo-lab-pc43/workspace/urban/cbd-slum/data/avg3m_small_24_07_2023.json"
+df_large_path =  "/Users/tagawayo-lab-pc43/workspace/urban/cbd-slum/data/avg3m_large_24_07_2023.json"
+volume_path = "/Users/tagawayo-lab-pc43/workspace/urban/cbd-slum/data/builtvolume_31_07_2023.json"
+city_s=gpd.read_file(df_small_path)
+city_l=gpd.read_file(df_large_path)
+city = gpd.GeoDataFrame( pd.concat([city_l,city_s])).set_index("ID_HDC_G0",drop=False)
+city_v = gpd.read_file(volume_path).set_index("ID_HDC_G0")
+
+city_v = city_v[city_v["totvol"] > 0  ]
+#city_v = city_v[city_v["P15"] > 1e5  ]
+#print(len(city_l))
+#print(len(city_s))
+#print(len(city))
+#city = city[city["P15"] > 1e5]
+#print(city.columns)
+#print(city["infpercap"].max())
+#print(city["avgh"].mean())
+#jkt = city[city["UC_NM_MN"] == "Jakarta"]
+#print(jkt["ID_HDC_G0"])
+#cellHP = read_compressed("./data/cell_files_uc/cell_"  + str(jkt.ID_HDC_G0.values[0]) + ".gz")
 #norm=matplotlib.colors.LogNorm(vmin=0.001,vmax=100)
-plt.figure()
-plt.scatter(city["FUA_p_2015"],city["cbd_a_30"], s=5,c= "r", label=r"$H_{\rm min} = 30 \rm m$")
-plt.scatter(city["FUA_p_2015"],city["cbd_a_40"], s=5,c="g", label=r"$H_{\rm min} = 40 \rm m$")
-plt.scatter(city["FUA_p_2015"],city["cbd_a_50"], s=5,c= "b",label=r"$H_{\rm min} = 50 \rm m$")
-plt.scatter(city["FUA_p_2015"],city["cbd_a_60"], s=5,c="c", label=r"$H_{\rm min} = 60 \rm m$")
-plt.scatter(city["FUA_p_2015"],city["cbd_a_70"], s=5,c= "m",label=r"$H_{\rm min} = 70 \rm m$")
-plt.xscale("log")
-plt.yscale("log")
-plt.xlabel("Population")
-plt.ylabel(r"Area of high-rise building ($\rm km^2$)")
-plt.legend()
-plt.savefig("h-vs-pop-log.png")
+#print(cellHP["Pval"].sum())
+#print(jkt["P15"])
+#city["sprdif"] = city["spr_pop"] - city["spr_h"]
+#city["sprdif_abs"] = np.abs(city["spr_pop"] - city["spr_h"])
+
+avg_spr_p = city["spr_pop"].mean()
+avg_spr_h = city["spr_h"].mean()
+
+city["h_poly"] = city["spr_h"] > avg_spr_h
+city["p_poly"] = city["spr_pop"] > avg_spr_p
+
+truedic = {True: "Poly", False: "Mono"}
+
+city["h_poly"] = city["h_poly"].map(truedic)
+city["p_poly"] = city["p_poly"].map(truedic)
+
+#print(city["h_poly"])
+
+#city.plot.hist(column="infpercap",by="p_poly", figsize=(10, 8))
+#plt.show()
+
+
+polys = city[city["p_poly"] == "Poly" ]
+monos = city[city["p_poly"] == "Mono"]
+
+
+mm, bm, r2m = fitfunc(np.log10(city_v["P15"]),np.log10(city_v["totvol"]))
+#mp, bp, r2p = fitfunc(np.log10(polys["P15"]),np.log10(polys["avgh"]))
+
+plt.scatter(np.log10(city_v["P15"]), np.log10(city_v["totvol"]),s=1)
+plt.show()
+
+print(mm)
+print(bm)
+
+#print(mp)
+#print(bp)
 
 plt.figure()
-plt.scatter(city["FUA_p_2015"],city["cbd_a_30"], s=5,c= "r", label=r"$H_{\rm min} = 30 \rm m$")
-plt.scatter(city["FUA_p_2015"],city["cbd_a_40"], s=5,c="g", label=r"$H_{\rm min} = 40 \rm m$")
-plt.scatter(city["FUA_p_2015"],city["cbd_a_50"], s=5,c= "b",label=r"$H_{\rm min} = 50 \rm m$")
-plt.scatter(city["FUA_p_2015"],city["cbd_a_60"], s=5,c="c", label=r"$H_{\rm min} = 60 \rm m$")
-plt.scatter(city["FUA_p_2015"],city["cbd_a_70"], s=5,c= "m",label=r"$H_{\rm min} = 70 \rm m$")
+plt.scatter(city["P15"], city["spr_h"],s=5)
 plt.xscale("log")
 #plt.yscale("log")
-plt.ylim(-0.1,1)
-plt.xlabel("Population")
-plt.ylabel(r"Area of high-rise building ($\rm km^2$)")
-plt.legend()
-plt.savefig("h-vs-pop.png")
+#plt.ylim(-0.1,1)
+plt.xlabel("Pop")
+plt.ylabel("spr h")
+#plt.legend()
+plt.savefig("spr_h.png")
 
 plt.figure()
-sc = plt.scatter(city["gini_pop"],city["gini_h"], s=5,c=city["FUA_p_2015"], norm=mpl.colors.LogNorm(vmin=1e5,vmax=1e8))
+plt.scatter(city["P15"], city["spr_pop"],s=5)
+plt.xscale("log")
+#plt.yscale("log")
+#plt.ylim(-0.1,1)
+plt.xlabel("Pop")
+plt.ylabel("spr p")
+#plt.legend()
+plt.savefig("spr_p.png")
+"""
+plt.figure()
+sc = plt.scatter(city["spr_pop"],city["spr_h"], s=5,c=city["P15"], norm=mpl.colors.LogNorm(vmin=1e5,vmax=1e8))
 plt.colorbar(sc)
 #plt.xscale("log")
 #plt.yscale("log")
 #plt.ylim(-0.1,1)
-plt.xlabel("Gini pop")
-plt.ylabel("Gini H")
+plt.xlabel("Spr pop")
+plt.ylabel("Spr H")
 #plt.legend()
-plt.savefig("ginih-vs-ginipop.png")
+plt.savefig("sprh-vs-sprpop.png")
 
+plt.figure()
+plt.scatter(city["P15"], city["sprdif"],s=5)
+plt.xscale("log")
+#plt.yscale("log")
+#plt.ylim(-0.1,1)
+plt.xlabel("Pop")
+plt.ylabel("Spr pop - spr h")
+#plt.legend()
+plt.savefig("sprdifvspop.png")
+
+plt.figure()
+plt.scatter(city["P15"], city["sprdif_abs"],s=5)
+plt.xscale("log")
+#plt.yscale("log")
+#plt.ylim(-0.1,1)
+plt.xlabel("Pop")
+plt.ylabel("Spr pop - spr h")
+#plt.legend()
+plt.savefig("sprdifvspop_abs.png")
+"""
 #tolong dibikin lebih fancy wk
---
+++ radial_scaling.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 13 20:42:02 2023
+
+@author: genta
+"""
+import itertools
+import geopandas as gpd
+import numpy as np
+from scipy.spatial import distance
+import osmnx
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def radial_center_point (start, num, data):
+    
+    target=[(x, y) for x, y in zip(data.x, data.y)]
+    center=data.dissolve().centroid
+    dismat=distance.cdist([(center.x[0], center.y[0])], target).T
+    first=start
+    for dist in np.arange(start, dismat.max(), num):
+        #print(dist)
+        where=np.where((dismat>first) & (dismat<dist))
+        local=dismat[where]
+        data.loc[where[0], "ovr"]=str(first)+"-"+str(dist)
+        first=dist
+    where=np.where((dismat>first))
+    data.loc[where[0], "ovr"]=str(first)+"-"+str(first+num)
+    return data
+def rsquare(x, y, degree):
+    results = {}
+    coeffs = np.polyfit(x, y, degree)
+     # Polynomial Coefficients
+    results['polynomial'] = coeffs.tolist()
+    # r-squared
+    p = np.poly1d(coeffs)
+    # fit values, and mean
+    yhat = p(x)                         # or [p(z) for z in x]
+    ybar = np.sum(y)/len(y)          # or sum(y)/len(y)
+    ssreg = np.sum((yhat-ybar)**2)   # or sum([ (yihat - ybar)**2 for yihat in yhat])
+    sstot = np.sum((y - ybar)**2)    # or sum([ (yi - ybar)**2 for yi in y])
+    rsq = ssreg / sstot
+    return rsq
+def fitfunc(x,y):
+    m,b = np.polyfit(x,y,1)
+    r2 = rsquare(x,y,1)
+    return m,b,r2
+
+def list_files(dir,ex="csv"):
+   import os
+   r=[]
+   for root, dirs, files in os.walk(dir):
+        for name in files:
+            if name.split(".")[-1]==ex:
+                r.append(os.path.join(root, name))
+   print(f"generating data from {len(r)} files")
+   return r
+
+def radial_scaling(datas, y_col, start, num):
+    glob={}
+    betas={}
+    for data in datas:
+        data=gpd.read_file(data)
+        data=radial_center_point (start, num, data)
+        grp=data.groupby("ovr")
+        local=grp[[y_col, "Pval"]].sum()
+        for i in local.index:
+            if i in glob.keys():
+                glob[i]=glob[i]+[local.loc[i]]
+            else:
+                glob[i]=[local.loc[i]]
+                
+    for k, v in glob.items():
+        if len(v)>100:
+            plotd=pd.concat([  pd.DataFrame(d).T for d in v]).reset_index()
+            plotd=plotd.dropna()
+            plotd=plotd.query(f"{y_col} > 0 and Pval>0")
+            x,y=np.log10(plotd[y_col]), np.log10(plotd["Pval"])
+            betas[k]=fitfunc(x,y)
+    betas=pd.DataFrame(betas).T
+    betas.columns =["m", "b", "r2"]    
+    return betas 
+
+
+start, num=0, 1000
+datas=list_files("/Volumes/HDPH-UT/K-Jkt copy/cbd-slum/data/cell_files/", "json")
+y_col="Hval"
+
+
+
+betas=radial_scaling(datas, y_col, start, num)
+betas["m"].plot()
+plt.show()
+betas.to_csv("betas_radial")
